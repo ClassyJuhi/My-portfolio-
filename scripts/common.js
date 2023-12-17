7b0210ad45c771d9eb9209a598c0fa848139b327
ClassyJuhi <1juhisingh1@gmail.com>
2023-12-17 09:21:06 +0530

    updated portfolio

--- scripts/common.js
+++ scripts/common.js
@@ -23,83 +23,6 @@ closeSidebar.addEventListener('click', () =>{
 })
 
 
-
-
-
-
-
-
-
-
-// document.addEventListener("DOMContentLoaded", function () {
-//     const header = document.querySelector(".animate");
-  
-//     // Function to check if an element is in the viewport
-//     function isInViewport(element) {
-//         const rect = element.getBoundingClientRect();
-//         return (
-//             rect.top >= 0 &&
-//             rect.left >= 0 &&
-//             rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-//             rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-//         );
-//     }
-  
-//     // Function to handle scroll events
-//     function handleScroll() {
-//         console.log("Scrolling");
-//         if (isInViewport(header)) {
-//             header.style.animationPlayState = "running";
-//         }
-//     }
-  
-//     // Add scroll event listener
-//     window.addEventListener("scroll", handleScroll);
-  
-//     // Initial check on page load
-//     handleScroll();
-//   });
-  
-
-
-  
-//   document.addEventListener("DOMContentLoaded", function () {
-//     const observerTargets = document.querySelectorAll('.animate');
-
-//     const observer = new IntersectionObserver(entries => {
-//         entries.forEach(entry => {
-//             if (entry.isIntersecting) {
-//                 entry.target.classList.add('animate-active');
-//                 observer.unobserve(entry.target);
-//             }
-//         });
-//     }, {
-//         root: null,
-//         threshold: 0.5,
-//     });
-
-//     observerTargets.forEach(target => {
-//         observer.observe(target);
-//     });
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 let sections = document.querySelectorAll('.animate');
 
 window.onscroll = () => {
@@ -107,7 +30,7 @@ window.onscroll = () => {
     let top = window.scrollY;
     let offset = sec.offsetTop;
     let height = sec.offsetHeight;
-    let halfway = window.innerHeight * 0.75; // 50% of the viewport height
+    let halfway = window.innerHeight * 0.85; // 50% of the viewport height
 
     if (top + halfway >= offset && top < offset + height) {
       sec.classList.add('show-animate');
